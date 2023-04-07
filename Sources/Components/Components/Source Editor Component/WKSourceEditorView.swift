35d01b108af6b5eef2f020adf9482b528075bb18
Toni Sevener <tsevener@wikimedia.org>
2023-04-07 10:40:06 -0500

    Adjust inset when keyboard displays

--- Sources/Components/Components/Source Editor Component/WKSourceEditorView.swift
+++ Sources/Components/Components/Source Editor Component/WKSourceEditorView.swift
@@ -34,6 +34,8 @@ class WKSourceEditorView: WKComponentView {
     // MARK: - Properties
     
     private let configuration: Configuration
+    
+    // MARK: - Lifecycle
 
     required init(configuration: Configuration) {
         self.configuration = configuration
@@ -41,20 +43,49 @@ class WKSourceEditorView: WKComponentView {
         setup()
     }
     
-    // MARK: - Lifecycle
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     private func setup() {
         addSubview(textView)
-
+        
         NSLayoutConstraint.activate([
-            leadingAnchor.constraint(equalTo: textView.leadingAnchor),
-            trailingAnchor.constraint(equalTo: textView.trailingAnchor),
-            topAnchor.constraint(equalTo: textView.topAnchor),
-            bottomAnchor.constraint(equalTo: textView.bottomAnchor)
+            safeAreaLayoutGuide.leadingAnchor.constraint(equalTo: textView.leadingAnchor),
+            safeAreaLayoutGuide.trailingAnchor.constraint(equalTo: textView.trailingAnchor),
+            safeAreaLayoutGuide.topAnchor.constraint(equalTo: textView.topAnchor),
+            safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: textView.bottomAnchor)
         ])
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillChangeFrame(_:)),
+                                               name: UIApplication.keyboardWillChangeFrameNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(_:)),
+                                               name: UIApplication.keyboardWillHideNotification,
+                                               object: nil)
+        
+        backgroundColor = .white
+    }
+    
+    // MARK: - Notifications
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        updateInsets(keyboardHeight: 0)
+    }
+
+    @objc private func keyboardWillChangeFrame(_ notification: Notification) {
+        if let frame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            let height = max(frame.height - safeAreaInsets.bottom, 0)
+            updateInsets(keyboardHeight: height)
+        }
+    }
+    
+    // MARK: - Private
+    
+    private func updateInsets(keyboardHeight: CGFloat) {
+        textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
+        textView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
     }
 }
