104efe52b338fbac7a0c16e1ba93bf36561c8b0d
Toni Sevener <tsevener@wikimedia.org>
2023-05-04 16:37:50 -0500

    Add a basic theme selection screen to demo app

--- Demo/Demo/RootWindow.swift
+++ Demo/Demo/RootWindow.swift
@@ -3,11 +3,21 @@ import Components
 
 /// The root window for the demo app. Trait collection changes are fed into the Components system here.
 final class RootWindow: UIWindow {
-
+    
+    override init(windowScene: UIWindowScene) {
+        super.init(windowScene: windowScene)
+        
+        WKAppEnvironment.updateWithTraitCollection(traitCollection)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 	override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
 		super.traitCollectionDidChange(previousTraitCollection)
 
-		WKAppEnvironment.current.set(theme: traitCollection.userInterfaceStyle == .light ? WKTheme.light : WKTheme.dark, traitCollection: traitCollection)
+        WKAppEnvironment.updateWithTraitCollection(traitCollection)
 	}
 
 }
