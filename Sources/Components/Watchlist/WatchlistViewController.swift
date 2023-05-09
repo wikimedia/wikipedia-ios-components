82078b9fffb15c6b36ae1210160628f202ede914
Toni Sevener <tsevener@wikimedia.org>
2023-05-09 11:35:23 -0500

    Add empty WatchlistViewController

---
+++ Sources/Components/Watchlist/WatchlistViewController.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+public final class WatchlistViewController: WKCanvasViewController {
+    
+    private let viewModel: WatchlistViewModel
+    
+    public init(viewModel: WatchlistViewModel) {
+        self.viewModel = viewModel
+        super.init()
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewModel.fetchWatchlist { result in
+            switch result {
+            case .success(let items):
+                print(items)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
