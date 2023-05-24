e327c6c55cea7f655eac036cb0faf22b93ffcfe3
Deepak Mantena <staykids@users.noreply.github.com>
2023-05-24 10:18:08 -0700

    Add `WKMenuButton` example.

--- Demo/Demo/RootWindow.swift
+++ Demo/Demo/RootWindow.swift
@@ -1,12 +1,23 @@
 import UIKit
+import Combine
 import Components
 
 /// The root window for the demo app. Trait collection changes are fed into the Components system here.
 final class RootWindow: UIWindow {
-    
+
+	// MARK: - Private Properties
+
+	private var cancellables = Set<AnyCancellable>()
+
+	// MARK: - Lifecycle
+
     override init(windowScene: UIWindowScene) {
         super.init(windowScene: windowScene)
-        
+
+		WKAppEnvironment.current.$theme.sink(receiveValue: { theme in
+			self.overrideUserInterfaceStyle = theme.userInterfaceStyle
+		}).store(in: &cancellables)
+
         WKAppEnvironment.updateWithTraitCollection(traitCollection)
     }
     
