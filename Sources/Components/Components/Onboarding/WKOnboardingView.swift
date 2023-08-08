3debcbb6b58130a487ab2a43613efb47bae628fe
mazevedo <mazevedo@wikimedia.org>
2023-08-08 18:37:02 -0300

    size class adjustemnts

--- Sources/Components/Components/Onboarding/WKOnboardingView.swift
+++ Sources/Components/Components/Onboarding/WKOnboardingView.swift
@@ -10,57 +10,64 @@ public struct WKOnboardingView: View {
 
     @ObservedObject var appEnvironment = WKAppEnvironment.current
 
+    @Environment (\.horizontalSizeClass) private var horizontalSizeClass
+
+    var sizeClassPadding: CGFloat {
+        horizontalSizeClass == .regular ? 64 : 32
+    }
+
     // MARK: - Lifecycle
 
     public var body: some View {
-        Group {
-            ScrollView {
-                VStack {
-                    Color(appEnvironment.theme.paperBackground)
-                        .ignoresSafeArea()
-                    Text(viewModel.title)
-                        .font(WKFont.for(.title).bold())
-                        .padding([.bottom, .top], 44)
-                        .multilineTextAlignment(.center)
-                    ForEach (1...viewModel.cells.count, id:\.self) { cell in
-                        VStack {
-                            WKOnboardingCell(viewModel: viewModel.cells[cell - 1])
-                                .padding([.bottom, .trailing], 20)
-                        }
-                    }
 
-                    Group {
+        ScrollView {
+            VStack {
+                Color(appEnvironment.theme.paperBackground)
+                    .ignoresSafeArea()
+                Text(viewModel.title)
+                    .font(WKFont.for(.title).bold())
+                    .padding([.bottom, .top], 44)
+                    .multilineTextAlignment(.center)
+                ForEach (1...viewModel.cells.count, id:\.self) { cell in
+                    VStack {
+                        WKOnboardingCell(viewModel: viewModel.cells[cell - 1])
+                            .padding([.bottom, .trailing], 20)
+                    }
+                }
+                Spacer()
+                VStack {
+                    Button(action: {
+                        mainButtonAction?()
+                    }, label: {
+                        Text(viewModel.mainButtonTitle)
+                    })
+                    .font(WKFont.for(.subheadline).bold())
+                    .foregroundColor(Color(appEnvironment.theme.paperBackground))
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 46)
+                    .background(Color(appEnvironment.theme.link))
+                    .cornerRadius(8)
+
+                    if let secondaryButton = viewModel.secondaryButtonTitle {
                         Button(action: {
-                            mainButtonAction?()
+                            secondaryButtonAction?()
                         }, label: {
-                            Text(viewModel.mainButtonTitle)
+                            Text(secondaryButton)
                         })
                         .font(WKFont.for(.subheadline).bold())
-                        .foregroundColor(Color(appEnvironment.theme.paperBackground))
+                        .foregroundColor(Color(appEnvironment.theme.link))
                         .frame(maxWidth: .infinity)
                         .frame(height: 46)
-                        .background(Color(appEnvironment.theme.link))
+                        .background(Color(appEnvironment.theme.paperBackground))
                         .cornerRadius(8)
 
-                        if let secondaryButton = viewModel.secondaryButtonTitle {
-                            Button(action: {
-                                secondaryButtonAction?()
-                            }, label: {
-                                Text(secondaryButton)
-                            })
-                            .font(WKFont.for(.subheadline).bold())
-                            .foregroundColor(Color(appEnvironment.theme.link))
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 46)
-                            .background(Color(appEnvironment.theme.paperBackground))
-                            .cornerRadius(8)
-
-                        }
                     }
-
                 }
-                .padding(32)
             }
+            .padding(sizeClassPadding)
+
+
         }
     }
+
 }
