fb99435629ceef49e4d38a62fb7adf63b10947c3
mazevedo <mazevedo@wikimedia.org>
2023-08-09 12:18:36 -0300

    Fix theming

--- Sources/Components/Components/Onboarding/WKOnboardingView.swift
+++ Sources/Components/Components/Onboarding/WKOnboardingView.swift
@@ -19,51 +19,52 @@ public struct WKOnboardingView: View {
     // MARK: - Lifecycle
 
     public var body: some View {
-        ScrollView(showsIndicators: true) {
-            VStack {
-                Color(appEnvironment.theme.paperBackground)
-                    .ignoresSafeArea()
-                Text(viewModel.title)
-                    .font(WKFont.for(.title).bold())
-                    .padding([.bottom, .top], 44)
-                    .multilineTextAlignment(.center)
-                ForEach (1...viewModel.cells.count, id:\.self) { cell in
-                    VStack {
-                        WKOnboardingCell(viewModel: viewModel.cells[cell - 1])
-                            .padding([.bottom, .trailing], 20)
-                    }
-                }
-                Spacer()
+        ZStack {
+            Color(appEnvironment.theme.paperBackground)
+                .ignoresSafeArea()
+            ScrollView(showsIndicators: true) {
                 VStack {
-                    Button(action: {
-                        mainButtonAction?()
-                    }, label: {
-                        Text(viewModel.mainButtonTitle)
-                    })
-                    .font(WKFont.for(.subheadline).bold())
-                    .foregroundColor(Color(appEnvironment.theme.paperBackground))
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 46)
-                    .background(Color(appEnvironment.theme.link))
-                    .cornerRadius(8)
-
-                    if let secondaryButton = viewModel.secondaryButtonTitle {
+                    Text(viewModel.title)
+                        .font(WKFont.for(.title).bold())
+                        .padding([.bottom, .top], 44)
+                        .multilineTextAlignment(.center)
+                    ForEach (1...viewModel.cells.count, id:\.self) { cell in
+                        VStack {
+                            WKOnboardingCell(viewModel: viewModel.cells[cell - 1])
+                                .padding([.bottom, .trailing], 20)
+                        }
+                    }
+                    Spacer()
+                    VStack {
                         Button(action: {
-                            secondaryButtonAction?()
+                            mainButtonAction?()
                         }, label: {
-                            Text(secondaryButton)
+                            Text(viewModel.mainButtonTitle)
                         })
                         .font(WKFont.for(.subheadline).bold())
-                        .foregroundColor(Color(appEnvironment.theme.link))
+                        .foregroundColor(Color(appEnvironment.theme.paperBackground))
                         .frame(maxWidth: .infinity)
                         .frame(height: 46)
-                        .background(Color(appEnvironment.theme.paperBackground))
+                        .background(Color(appEnvironment.theme.link))
                         .cornerRadius(8)
+
+                        if let secondaryButton = viewModel.secondaryButtonTitle {
+                            Button(action: {
+                                secondaryButtonAction?()
+                            }, label: {
+                                Text(secondaryButton)
+                            })
+                            .font(WKFont.for(.subheadline).bold())
+                            .foregroundColor(Color(appEnvironment.theme.link))
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 46)
+                            .background(Color(appEnvironment.theme.paperBackground))
+                            .cornerRadius(8)
+                        }
                     }
                 }
+                .padding(sizeClassPadding)
             }
-            .padding(sizeClassPadding)
         }
     }
-
 }
