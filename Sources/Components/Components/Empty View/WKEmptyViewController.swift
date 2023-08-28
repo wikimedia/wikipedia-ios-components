d037b553d7fcd88dd069116963c453f92736bc5c
mazevedo <mazevedo@wikimedia.org>
2023-08-28 13:29:46 -0300

    Add empty view controller

---
+++ Sources/Components/Components/Empty View/WKEmptyViewController.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+final class WKEmptyViewController: WKCanvasViewController {
+
+    var hostingController: WKEmptyViewHostingController
+
+    public init(viewModel: WKEmptyViewModel) {
+        self.hostingController = WKEmptyViewHostingController(viewModel: viewModel)
+        super.init()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+            addComponent(hostingController, pinToEdges: true)
+    }
+
+}
+
+final class WKEmptyViewHostingController: WKComponentHostingController<WKEmptyView> {
+
+    init(viewModel: WKEmptyViewModel) {
+        super.init(rootView: WKEmptyView(viewModel: viewModel))
+
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
