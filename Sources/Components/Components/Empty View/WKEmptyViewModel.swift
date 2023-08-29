5f8075de8e6a8a820c61879e50125577a979ecdf
mazevedo <mazevedo@wikimedia.org>
2023-08-29 13:26:26 -0300

    View model updates

--- Sources/Components/Components/Empty View/WKEmptyViewModel.swift
+++ Sources/Components/Components/Empty View/WKEmptyViewModel.swift
@@ -7,17 +7,25 @@ struct WKEmptyViewModel {
         case filter
     }
 
+    struct EmptyStateFilterSubtitleModel {
+        var modifyString: String
+        var filterString: String
+        var viewMoreString: String
+    }
+
     var image: UIImage
     var title: String
     var subtitle: String
+    var filterSubtitle: EmptyStateFilterSubtitleModel
     var buttonTitle: String?
     var type: EmptyStateType
     var activeFilters: Int?
 
-    init(image: UIImage, title: String, subtitle: String, buttonTitle: String?, type: EmptyStateType, activeFilters: Int? = nil) {
+    init(image: UIImage, title: String, subtitle: String, filterSubtitle: EmptyStateFilterSubtitleModel, buttonTitle: String?, type: EmptyStateType, activeFilters: Int? = nil) {
         self.image = image
         self.title = title
         self.subtitle = subtitle
+        self.filterSubtitle = filterSubtitle
         self.buttonTitle = buttonTitle
         self.type = type
         self.activeFilters = activeFilters
